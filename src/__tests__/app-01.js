ca5b6f10e02eef2dd23b77f9bb1911c926a21ec4
caseycameron <mcasey.cameron@gmail.com>
2022-07-20 11:00:53 -0700

    app-01 notes

--- src/__tests__/app-01.js
+++ src/__tests__/app-01.js
@@ -9,6 +9,7 @@ import App from '../app'
 jest.mock('../api')
 
 test('Can fill out a form across multiple pages', async () => {
+  // this is what our server would respond with
   mockSubmitForm.mockResolvedValueOnce({success: true})
   const testData = {food: 'test food', drink: 'test drink'}
   render(<App />)
@@ -16,6 +17,7 @@ test('Can fill out a form across multiple pages', async () => {
   fireEvent.click(screen.getByText(/fill.*form/i))
 
   fireEvent.change(screen.getByLabelText(/food/i), {
+    // the change event target value should be 'test food'
     target: {value: testData.food},
   })
   fireEvent.click(screen.getByText(/next/i))
@@ -24,12 +26,14 @@ test('Can fill out a form across multiple pages', async () => {
     target: {value: testData.drink},
   })
   fireEvent.click(screen.getByText(/review/i))
-
+  // this is the confirmation/review page
   expect(screen.getByLabelText(/food/i)).toHaveTextContent(testData.food)
   expect(screen.getByLabelText(/drink/i)).toHaveTextContent(testData.drink)
 
+  // this getByText picks up two instances of confirm on the page so scope it by css selector: button
   fireEvent.click(screen.getByText(/confirm/i, {selector: 'button'}))
-
+  // when the form is submitted ensure it was called once with testData
+  // mock this because this api form submit is asynchronous
   expect(mockSubmitForm).toHaveBeenCalledWith(testData)
   expect(mockSubmitForm).toHaveBeenCalledTimes(1)
 
