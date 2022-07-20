018fe22cc5116e75f1400ffe7b82c139742c25d0
caseycameron <mcasey.cameron@gmail.com>
2022-07-20 09:35:30 -0700

    custom-hook-01 green

--- src/__tests__/custom-hook-01.js
+++ src/__tests__/custom-hook-01.js
@@ -1,4 +1,20 @@
+import * as React from 'react'
+import {render, act} from '@testing-library/react'
+import {useCounter} from '../use-counter'
 
 test('exposes the count and increment/decrement functions', () => {
-  
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
+  render(<TestComponent />)
+  expect(result.count).toBe(0)
+  // here a state update was happening outside of act so we wrapped it
+  // increment calls a state updater in the use-counter component
+  // there's nothing that rtl can expose to wrap that in act so we do it manually
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
